c2d6504b0ff29f4faa622502979e041171d6b776
mahoromarlene <uwamahoromarlene@gmail.com>
2019-04-30 18:02:28 +0200

    started categories page

--- components/reusable/Cart.js
+++ components/reusable/Cart.js
@@ -1,4 +1,4 @@
-
+import Select2 from 'react-select2-wrapper';
 
 const SidebarUI = ({ isOpen, ...rest }) => {
     const classes = [
@@ -62,43 +62,99 @@ class Cart extends React.Component {
                             <div className="main-title">
                                 <div className='sidebar-title'>
                                     <span className='icon-Path-63'></span>
-                                    <span>My cart</span>
+                                    <span className='title'>My cart</span>
+                                    <span className='item-nbr'>(7 Items Total)</span>
                                 </div>
-                                <div className='sidebar-close'>
-                                    <span>Close</span><span className="icon-Path-58" onClick={() => this.openSidebar(false)} />
+                                <div className='sidebar-close' onClick={() => this.openSidebar(false)}>
+                                    <span className='close-cart'>Close</span><span className="icon-Path-58" />
                                 </div>
                             </div>
                             <div className='cart-content'>
-                                <div className='store-logo'>
-                                    <img className='store-img' src='https://res.cloudinary.com/hehe/image/upload/v1556288678/multi-vendor/shop-icon-4_2x.png' />
-                                    <span className='store-name'>Mart</span>
+                                <div className='cart-store-content'>
+                                    <div className='store-logo'>
+                                        <img className='store-img' src='https://res.cloudinary.com/hehe/image/upload/v1556288678/multi-vendor/shop-icon-4_2x.png' />
+                                        <span className='store-name'>Mart (3 Items from Mart store)</span>
+                                    </div>
+                                    <div className='row reset-row cart-item'>
+                                        <div className='col-lg-2 col-md-2 col-sm-2 col-2 col-reset product-cart-image'>
+                                            <img className='cart-product-img' src='https://res.cloudinary.com/hehe/image/upload/v1556288623/multi-vendor/prod_1_2x.png' />
+                                        </div>
+                                        <div className='col-lg-5 col-md-5 col-sm-5 col-5 col-reset name-incremenet'>
+                                            <div className='product-name'>Cabagge</div>
+                                            <div className='qty-measurement'>Pce</div>
+                                            <div className='qty-increment'>
+                                                <span className='decrement'>-</span>
+                                                <span className='number'>12</span>
+                                                <span className='increment'>+</span>
+                                            </div>
+                                        </div>
+                                        <div className='col-lg-5 col-md-5 col-sm-5 col-5 col-reset prices-discounts'>
+                                            <div className='cart-bk'>
+                                                <div className='discount'>10% OFF</div>
+                                                <div />
+                                            </div>
+                                            <div className='cart-bk'>
+                                                <div className='edit'><span className='icon-Path-68'></span>Edit</div>
+                                                <div className='initial-price'>Rwf 1000</div>
+                                            </div>
+                                            <div className='cart-bk'>
+                                                <div className='remove'><span className='icon-Path-60'></span>Remove</div>
+                                                <div className='price'>Rwf 900</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className='row reset-row cart-item'>
+                                        <div className='col-lg-2 col-md-2 col-sm-2 col-2 col-reset product-cart-image'>
+                                            <img className='cart-product-img' src='https://res.cloudinary.com/hehe/image/upload/v1556288623/multi-vendor/prod_1_2x.png' />
+                                        </div>
+                                        <div className='col-lg-5 col-md-5 col-sm-5 col-5 col-reset name-incremenet'>
+                                            <div className='product-name'>Cabagge</div>
+                                            <div className='qty-measurement'>Pce</div>
+                                            <div className='qty-increment'>
+                                                <span className='decrement'>-</span>
+                                                <span className='number'>12</span>
+                                                <span className='increment'>+</span>
+                                            </div>
+                                        </div>
+                                        <div className='col-lg-5 col-md-5 col-sm-5 col-5 col-reset prices-discounts'>
+                                            <div className='cart-bk'>
+                                                <div className='discount'>10% OFF</div>
+                                                <div />
+                                            </div>
+                                            <div className='cart-bk'>
+                                                <div className='edit'><span className='icon-Path-68'></span>Edit</div>
+                                                <div className='initial-price'>Rwf 1000</div>
+                                            </div>
+                                            <div className='cart-bk'>
+                                                <div className='remove'><span className='icon-Path-60'></span>Remove</div>
+                                                <div className='price'>Rwf 900</div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div className='row reset-row'>
-                                    <div className='col-lg-2 col-md-2 col-sm-2 col-2 col-reset'>
-                                        <img className='cart-product-img' src='https://res.cloudinary.com/hehe/image/upload/v1556288623/multi-vendor/prod_1_2x.png' />
+                                <div className='total-price'>
+                                    <div className='subtotal'>Sutotal: Rwf 21,345</div>
+                                    <div className='shipping-grid'>
+                                        <span className='shipping-title'>Shipping method</span>
+                                        <span className='shipping-dropdown'>
+                                            <Select2
+                                                defaultValue={2}
+                                                data={[
+                                                    { text: 'WHS', id: 1 },
+                                                    { text: 'another WHS', id: 2 },
+                                                    { text: 'other WHS', id: 3}
+                                                ]}
+                                            />
+                                        </span>
+                                        <span className='shipping'>Total: Rwf 21,345</span>
                                     </div>
-                                    <div className='col-lg-5 col-md-5 col-sm-5 col-5 col-reset'>
-                                        <div className='product-name'>Cabagge</div>
-                                        <div className='qty-measurement'>Pce</div>
-                                        <div className='qty-increment'>
-                                            <span className='decrement'>-</span>
-                                            <span className='number'>12</span>
-                                            <span className='increment'>+</span>
-                                        </div>
+                                    <div className='total-grid'>
+                                        <span className='total'>Total: Rwf 45,475</span>
                                     </div>
-                                    <div className='col-lg-5 col-md-5 col-sm-5 col-5 col-reset'>
-                                        <div className='cart-bk'>
-                                            <span className='discount'>10% OFF</span>
-                                        </div>
-                                        <div className='cart-bk'>
-                                            <div><span className='icon-Path-68'></span>Edit</div>
-                                            <div>Rwf 1000</div>
-                                        </div>
-                                        <div className='cart-bk'>
-                                            <div><span className='icon-Path-60'></span>Remove</div>
-                                            <div>Rwf 900</div>
-                                        </div>
-                                    </div>
+                                </div>
+                                <div className='go-to-checkout'>
+                                    <span className='grand-total'>All Total: Rwf 70,000</span>
+                                    <span className='proceed-button'><button>Proceed to Checkout</button></span>
                                 </div>
                             </div>
                         </div>
