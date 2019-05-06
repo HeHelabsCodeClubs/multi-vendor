a2c5ea59e08b701617b4863aaa4f5622a92086a2
mahoromarlene <uwamahoromarlene@gmail.com>
2019-05-06 15:37:19 +0200

    product popup

--- components/reusable/Product.js
+++ components/reusable/Product.js
@@ -1,6 +1,8 @@
 import Link from 'next/link'; 
 import Modal from 'react-responsive-modal';
+import Select2 from 'react-select2-wrapper';
 import '../../assets/styles/layouts/product.scss';
+import ProductPopup from '../views/ProductPopup';
 
 class Product extends React.Component {
     constructor(props) {
@@ -8,57 +10,54 @@ class Product extends React.Component {
         this.state = {
             open: false
         };
+        this.onOpenModal = this.onOpenModal.bind(this);
+        this.onCloseModal = this.onCloseModal.bind(this);
     }
-    onOpenModal = () => {
-        this.setState({ open: true });
+    onOpenModal() {
+        this.setState({ 
+            open: true 
+        });
     };
 
-    onCloseModal = () => {
-        this.setState({ open: false });
+    onCloseModal () {
+        this.setState({ 
+            open: false 
+        });
     };
 	render() {
         const { open } = this.state;
 		return (
-			<div className='single-product' onClick={this.onOpenModal}>
-                <Link href=''>
-                    <a>
-                        <img className='product-img' src='https://res.cloudinary.com/hehe/image/upload/q_auto,f_auto,fl_lossy/v1556288624/multi-vendor/prod_3_2x.png' />
-                        <div className='top-icons'>
-                            <span className='hot'><span className='icon-Path-54'></span>hot</span>
-                            <span className='discount'>50% OFF</span>
-                        </div>
-                        <div className='product-description'>
-                            <div className='store-logo'>
-                                <img className='store-img' src='https://res.cloudinary.com/hehe/image/upload/q_auto,f_auto,fl_lossy/v1556288678/multi-vendor/shop-icon-4_2x.png' />
-                                <span className='store-name'>Mart</span>
+            <div>
+                <div className='single-product' onClick={this.onOpenModal}>
+                    <Link href=''>
+                        <a>
+                            <img className='product-img' src='https://res.cloudinary.com/hehe/image/upload/q_auto,f_auto,fl_lossy/v1556288624/multi-vendor/prod_3_2x.png' />
+                            <div className='top-icons'>
+                                <span className='hot'><span className='icon-Path-54'></span>hot</span>
+                                <span className='discount'>50% OFF</span>
                             </div>
-                            <div className='product-name'>Full detailed Product title - and info about it... for home use only</div>
-                            <div className='price-cart'>
-                                <span>
-                                    <span className='price'>Rwf 5000</span>
-                                    <span className='initial-price'>Rwf 10000</span>
-                                </span>
-                                <span className='add-to-cart'>
-                                    <button><span className='icon-Path-63'></span></button>
-                                </span>
+                            <div className='product-description'>
+                                <div className='store-logo'>
+                                    <img className='store-img' src='https://res.cloudinary.com/hehe/image/upload/q_auto,f_auto,fl_lossy/v1556288678/multi-vendor/shop-icon-4_2x.png' />
+                                    <span className='store-name'>Mart</span>
+                                </div>
+                                <div className='product-name'>Full detailed Product title - and info about it... for home use only</div>
+                                <div className='price-cart'>
+                                    <span>
+                                        <span className='price'>Rwf 5000</span>
+                                        <span className='initial-price'>Rwf 10000</span>
+                                    </span>
+                                    <span className='add-to-cart'>
+                                        <button><span className='icon-Path-63'></span></button>
+                                    </span>
+                                </div>
                             </div>
-                        </div>
-                    </a>
-                </Link>  
+                        </a>
+                    </Link>            
+                </div>
                 <Modal open={open} onClose={this.onCloseModal} center>
-                    <div className='popup-wrapper'>
-                        <div className='product-wrapper'>
-                            <div className='row'>
-                                <div className='col-lg-5 col-md-5 col-sm-5 col-12'>
-                                    me
-                                </div>
-                                <div className='col-lg-7 col-md-7 col-sm-7 col-12'>
-                                        you
-                                </div>
-                            </div>  
-                        </div>
-                    </div>
-                </Modal>          
+                    <ProductPopup />
+                </Modal>
             </div>
 		);
 	}
