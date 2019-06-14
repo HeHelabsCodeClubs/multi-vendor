fc51970588e103f80bad4450500d6db811497184
christophe12 <mutachristophe12@gmail.com>
2019-06-14 11:29:49 +0200

    finalized first interaction with the categories page

--- server.js
+++ server.js
@@ -32,6 +32,14 @@ app
             });
         })
 
+        server.get('/categories/:category_slug/:sub_cat_slug/:sub_last_cat_slug', (req, res) => {
+            return app.render(req, res, '/categories', { 
+                category_slug: req.params.category_slug,
+                sub_cat_slug: req.params.sub_cat_slug,
+                sub_last_cat_slug: req.params.sub_last_cat_slug
+            });
+        })
+
         server.get('/p/:id', (req, res) => {
             const actualPage = '/post'
             const queryParams = { title: req.params.id }
