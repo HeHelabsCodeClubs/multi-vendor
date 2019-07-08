b4391ed2dca31faaf086cc4bcae94c91a42104c4
plantejosue <plantejosh1@gmail.com>
2019-07-08 18:32:30 +0200

    rough draft order successful/failure

--- components/views/orderResponse/OrderFailure.js
+++ components/views/orderResponse/OrderFailure.js
@@ -9,26 +9,28 @@ import '../../../assets/styles/layouts/orderResponse.scss';
 class OrderFailure extends Component {
     render() {
         return (
-            <div className="order-response__wrapper failure">
-                <div>
-                    <h1>Sorry</h1>
-                </div>
-                <div>
-                    <h5>Your payment Failed</h5>
-                    <p>Something went wrong! Please make sure you filled out</p>
-                    <p>true and try again!</p>
-                </div>
-                <ul>
-                    <li><a href="#">Try again</a></li>
-                    <li>or Go to <a href="/order">Home page</a></li>
-                </ul>
-                <div className="site-logo">
-                    <a href='/'>
-                        <img src='https://res.cloudinary.com/hehe/image/upload/v1556120378/multi-vendor/HeHe_Logo_Original_landscape.svg' />
-                    </a>
+            <div className='landing-wrapper failure-content'>
+                <div className='auth-content'>
+                    <div>
+                        <h1>Sorry</h1>
+                    </div>
+                    <div>
+                        <h5>Your payment Failed</h5>
+                        <p>Something went wrong! Please make sure you filled out</p>
+                        <p>true and try again!</p>
+                    </div>
+                    <ul>
+                        <li><a href="#">Try again</a></li>
+                        <li>or Go to <a href="/order">Home page</a></li>
+                    </ul>
+                    <div className="site-logo">
+                        <a href='/'>
+                            <img src='https://res.cloudinary.com/hehe/image/upload/v1556120378/multi-vendor/HeHe_Logo_Original_landscape.svg' />
+                        </a>
+                    </div>
                 </div>
             </div>
-        )
+        );
     }
 }
 
--- components/views/orderResponse/OrderSuccess.js
+++ components/views/orderResponse/OrderSuccess.js
@@ -9,23 +9,25 @@ import '../../../assets/styles/layouts/orderResponse.scss';
 class OrderSuccess extends Component {
     render() {
         return (
-            <div className="order-response__wrapper success">
-                <div>
-                    <h1>Thank You</h1>
-                </div>
-                <div>
-                    <h5>Your payment is successfull</h5>
-                    <p>Thank yo for shopping with us! An automated receipt will</p>
-                    <p>be sent on your registered email</p>
-                </div>
-                <ul>
-                    <li><a href="/">Go back home</a></li>
-                    <li>or Go to <a href="/order"> Orders page</a></li>
-                </ul>
-                <div className="site-logo">
-                    <a href='/'>
-                        <img src='https://res.cloudinary.com/hehe/image/upload/v1556120378/multi-vendor/HeHe_Logo_Original_landscape.svg' />
-                    </a>
+            <div className='landing-wrapper success-content'>
+                <div className='auth-content'>
+                    <div>
+                        <h1>Thank You</h1>
+                    </div>
+                    <div>
+                        <h5>Your payment is successfull</h5>
+                        <p>Thank yo for shopping with us! An automated receipt will</p>
+                        <p>be sent on your registered email</p>
+                    </div>
+                    <ul>
+                        <li><a href="/">Go back home</a></li>
+                        <li>or Go to <a href="/order"> Orders page</a></li>
+                    </ul>
+                    <div className="site-logo">
+                        <a href='/'>
+                            <img src='https://res.cloudinary.com/hehe/image/upload/v1556120378/multi-vendor/HeHe_Logo_Original_landscape.svg' />
+                        </a>
+                    </div>
                 </div>
             </div>
         )
