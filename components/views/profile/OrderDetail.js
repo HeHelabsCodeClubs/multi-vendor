4cacf919f2f885b8676a5f840a4d54caa5779a82
plantejosue <plantejosh1@gmail.com>
2019-07-10 09:46:12 +0200

    order detail

--- components/views/profile/OrderDetail.js
+++ components/views/profile/OrderDetail.js
@@ -4,7 +4,7 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 class OrderDetail extends Component {
     render () {
         return (
-            <div>
+            <div className="order-detail">
                 <div className="content-header">
                     <h5>Order #0015</h5>
                 </div>
@@ -66,19 +66,122 @@ class OrderDetail extends Component {
 
                             <div className="col-lg-3 col-md-3 col-sm-6 col-6">
                                 <div className="card-content">
-                                    <p></p>
-                                    <h5></h5>
+                                    <p>Delivery status</p>
+                                    <h5>DELIVERED</h5>
                                 </div>
                                 <div className="card-content">
-                                    <p></p>
-                                    <h5></h5>
+                                    <p>Billing address</p>
+                                    <h5>Same as billing addess</h5>
                                 </div>
                             </div>
                         </div>
 
+                        <div className='delivery-content'>
+                            <div className='store-logo'>
+                                <img className='store-img' src="" />
+                                <span className='store-name'>
+                                    <span className='name'>Mart</span>(3 Items from Hmart store)
+                                </span>
+                            </div>
+                            <div className="table-wrapper">                
+                                <table>
+                                    <thead>
+                                        <th>Order items</th>
+                                        <th>Price</th>
+                                        <th>Qty</th>
+                                        <th>Total Price</th>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                <tr>
+                                                    <td><img src="https://res.cloudinary.com/hehe/image/upload/v1559573668/multi-vendor/products/1/vro0lorshxiasgf8eg9d.jpg" /></td>
+                                                    <td>Zara<span>Pair</span></td>
+                                                </tr>
+                                            </td>                                            
+                                            <td>Rwf 6800</td>
+                                            <td>12</td>                                            
+                                            <td>Rwf 6800</td>
+                                        </tr>
 
+                                        <tr>
+                                            <td>
+                                                <tr>
+                                                    <td><img src="https://res.cloudinary.com/hehe/image/upload/v1559573668/multi-vendor/products/1/vro0lorshxiasgf8eg9d.jpg" /></td>
+                                                    <td>Zara<span>Pair</span></td>
+                                                </tr>
+                                            </td>                                            
+                                            <td>Rwf 6800</td>
+                                            <td>12</td>                                            
+                                            <td>Rwf 6800</td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>
+                                                <tr>
+                                                    <td><img src="https://res.cloudinary.com/hehe/image/upload/v1559573668/multi-vendor/products/1/vro0lorshxiasgf8eg9d.jpg" /></td>
+                                                    <td>Zara<span>Pair</span></td>
+                                                </tr>
+                                            </td>                                            
+                                            <td>Rwf 6800</td>
+                                            <td>12</td>                                            
+                                            <td>Rwf 6800</td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>
+                                                <tr>
+                                                    <td><img src="https://res.cloudinary.com/hehe/image/upload/v1559573668/multi-vendor/products/1/vro0lorshxiasgf8eg9d.jpg" /></td>
+                                                    <td>Zara<span>Pair</span></td>
+                                                </tr>
+                                            </td>                                            
+                                            <td>Rwf 6800</td>
+                                            <td>12</td>                                            
+                                            <td>Rwf 6800</td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>
+                                                <tr>
+                                                    <td><img src="https://res.cloudinary.com/hehe/image/upload/v1559573668/multi-vendor/products/1/vro0lorshxiasgf8eg9d.jpg" /></td>
+                                                    <td>Zara<span>Pair</span></td>
+                                                </tr>
+                                            </td>                                            
+                                            <td>Rwf 6800</td>
+                                            <td>12</td>                                            
+                                            <td>Rwf 6800</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+
+                            <div className='total-price'>
+                                <div className='subtotal'>
+                                    <span className='t-title'>Subtotal: </span>
+                                    <span className='t-content'>Rwf 21050</span>	
+                                </div>
+                                <div className='shipping-grid'>
+                                    <span className='shipping-title'>Shipping method</span>
+                                    <span className='shipping-dropdown'>
+                                        WHS internatianal
+                                    </span>
+                                    Shipping: Rwf 1050
+                                </div>
+                                <div className='total-grid checkout-total-grid row reset-row'>
+                                    Delivery time 6 days
+                                    <span className='total'>
+                                        <span className='t-title'>Total: </span>
+                                        <span className='t-content'>Rwf 21050</span>
+                                    </span>
+                                </div>
+                            </div>						
+                        </div>
 
                     </TabPanel>
+
+
+
+
                     <TabPanel>
                         <h2>Any content 2</h2>
                     </TabPanel>
