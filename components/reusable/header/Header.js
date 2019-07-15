a8cb765db102c4f4f6276bc065797238d21117bd
plantejosue <plantejosh1@gmail.com>
2019-07-15 09:46:45 +0200

    some changes

--- components/reusable/header/Header.js
+++ components/reusable/header/Header.js
@@ -5,13 +5,16 @@ import HeaderCategoryMenu from './HeaderCategoryMenu';
 import { getUserAuthenticatedInfo, logoutUser } from '../../../helpers/auth';
 import isObjectEmpty from '../../../helpers/is_object_empty';
 import SearchDropdown from './SearchDropdown';
+import classnames from "classnames";
 
 class Header extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             updateCart: false,
-            authUser: {}
+            authUser: {},
+            prevScrollpos: 0,//window.pageYOffset,
+            visible: true
         };
         this.cartShouldUpdate = this.cartShouldUpdate.bind(this);
         this.renderUserProfile = this.renderUserProfile.bind(this);
@@ -19,9 +22,13 @@ class Header extends React.Component {
         this.logOut = this.logOut.bind(this);
     }
     componentDidMount() {
+        this.setState({
+            prevScrollpos: window.pageYOffset,
+        })
         getUserAuthenticatedInfo((user) => {
             this.updateAuthUser(user);
         });
+        window.addEventListener("scroll", this.handleScroll);
     }
     componentWillReceiveProps(nextProps) {
         const { updateCart } = nextProps;
@@ -48,6 +55,25 @@ class Header extends React.Component {
         }
         logoutUser();
     }
+
+    // Hide menu on scroll      
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.handleScroll);
+    }
+    
+    handleScroll = () => {
+        const { prevScrollpos } = this.state;
+      
+        const currentScrollPos = window.pageYOffset;
+        const visible = prevScrollpos > currentScrollPos;
+      
+        this.setState({
+          prevScrollpos: currentScrollPos,
+          visible
+        });
+    };
+
+   
 
     // test deploy
 
@@ -87,78 +113,83 @@ class Header extends React.Component {
 
     render() {
         return (
-            <div>
-                <div className='top-panel'>
-                    <div className='row maximum-width'>
-                        <div className='col-lg-6 col-md-6 col-sm-6 col-12'>
-                            <div className='top-panel-left'>
-                                <span className='top-content'>+(250) 788 317 916</span>
-                                <span className='top-content'>order.hehe.rw</span>
+            <div className="header-panel">
+                <div className={classnames("nav__bar", {
+                    "navbar__hidden": !this.state.visible
+                })}
+                >
+                    <div className='top-panel'>
+                        <div className='row maximum-width'>
+                            <div className='col-lg-6 col-md-6 col-sm-6 col-12'>
+                                <div className='top-panel-left'>
+                                    <span className='top-content'>+(250) 786 456 686</span>
+                                    <span className='top-content'>order.hehe.rw</span>
+                                </div>
                             </div>
-                        </div>
-                        <div className='col-lg-6 col-md-6 col-sm-6 col-12'>
-                            <div className='top-panel-right'>
-                                <a href="https://tracking.wherehouseshipping.com/" target="_blank"><span className='top-content'>Track your order</span></a>
-                                <a href="https://hehe.rw/" target="_blank"><span className='top-content'>about</span></a>
-                                <a href="https://hehe.rw/#contactUs" target="_blank"><span className='top-content'>contact</span></a>
+                            <div className='col-lg-6 col-md-6 col-sm-6 col-12'>
+                                <div className='top-panel-right'>
+                                    <a href="https://tracking.wherehouseshipping.com/" target="_blank"><span className='top-content'>Track your order</span></a>
+                                    <a href="https://hehe.rw/" target="_blank"><span className='top-content'>about</span></a>
+                                    <a href="https://hehe.rw/#contactUs" target="_blank"><span className='top-content'>contact</span></a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className='header'>
-                    <div className='row maximum-width'>
-                        <div className='col-lg-4 col-md-4 col-sm-6 col-12 header-left'>
-                            <span className='site-logo'>
-                                <a href='/'>
-                                    <img src='https://res.cloudinary.com/hehe/image/upload/v1556120378/multi-vendor/HeHe_Logo_Original_landscape.svg' />
-                                </a>
-                            </span>
-                            <span className='location-dropdown'>
-                                <Select2
-                                    defaultValue={2}
-                                    data={[
-                                        { text: 'Kigali, Kacyiru', id: 1 },
-                                        { text: 'Kigali, Kimironko', id: 2 },
-                                        { text: 'Kigali, Kanombe', id: 3},
-                                        { text: 'Kigali, Town', id: 4 }
-                                    ]}
-                                />
-                            </span>
-                            <HeaderCategoryMenu />
-                        </div>
-                        <div className='col-lg-5 col-md-5 col-sm-2 col-3 search-container'>
-                            <span className='main-search'>
-
-                                <SearchDropdown />
-                                {/* <input type="text" placeholder="Search store or product" /> */}
-                                <span className='categories-dropdown'>
+                    <div className='header'>
+                        <div className='row maximum-width'>
+                            <div className='col-lg-4 col-md-4 col-sm-6 col-12 header-left'>
+                                <span className='site-logo'>
+                                    <a href='/'>
+                                        <img src='https://res.cloudinary.com/hehe/image/upload/v1556120378/multi-vendor/HeHe_Logo_Original_landscape.svg' />
+                                    </a>
+                                </span>
+                                <span className='location-dropdown'>
                                     <Select2
                                         defaultValue={2}
                                         data={[
-                                            { text: 'Shoes', id: 1 },
-                                            { text: 'Groceries', id: 2 },
-                                            { text: 'Clothes', id: 3},
-                                            { text: 'Electronics', id: 4 }
+                                            { text: 'Kigali, Kacyiru', id: 1 },
+                                            { text: 'Kigali, Kimironko', id: 2 },
+                                            { text: 'Kigali, Kanombe', id: 3},
+                                            { text: 'Kigali, Town', id: 4 }
                                         ]}
                                     />
                                 </span>
-                                <button type="submit"><span className="icon-Path-64"></span></button>
-                            </span>
-                        </div>
-                        <div className='col-lg-3 col-md-3 col-sm-4 col-3 account-container'>
-                            <div className='row row-container'>
-                                <div className='col-lg-6 col-md-6 col-sm-6 col-2 account-grid'>
-                                    <div className='row'>
-                                        <div className='col-lg-3 col-md-3 col-sm-3 col-12 col-reset'>
-                                            <span className='icon-Path-62'></span>
+                                <HeaderCategoryMenu />
+                            </div>
+                            <div className='col-lg-5 col-md-5 col-sm-2 col-3 search-container'>
+                                <span className='main-search'>
+
+                                    <SearchDropdown />
+                                    {/* <input type="text" placeholder="Search store or product" /> */}
+                                    <span className='categories-dropdown'>
+                                        <Select2
+                                            defaultValue={2}
+                                            data={[
+                                                { text: 'Shoes', id: 1 },
+                                                { text: 'Groceries', id: 2 },
+                                                { text: 'Clothes', id: 3},
+                                                { text: 'Electronics', id: 4 }
+                                            ]}
+                                        />
+                                    </span>
+                                    <button type="submit"><span className="icon-Path-64"></span></button>
+                                </span>
+                            </div>
+                            <div className='col-lg-3 col-md-3 col-sm-4 col-3 account-container'>
+                                <div className='row row-container'>
+                                    <div className='col-lg-6 col-md-6 col-sm-6 col-2 account-grid'>
+                                        <div className='row'>
+                                            <div className='col-lg-3 col-md-3 col-sm-3 col-12 col-reset'>
+                                                <span className='icon-Path-62'></span>
+                                            </div>
+                                            {/* user profile goes here */}
+                                            {this.renderUserProfile()}
                                         </div>
-                                        {/* user profile goes here */}
-                                        {this.renderUserProfile()}
                                     </div>
+                                    <Cart 
+                                    updateCart={this.state.updateCart}
+                                    />
                                 </div>
-                                <Cart 
-                                updateCart={this.state.updateCart}
-                                />
                             </div>
                         </div>
                     </div>
