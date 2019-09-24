c6dbeb8f630d5a9997a365697ff40405b07b42db
christophe12 <mutachristophe12@gmail.com>
2019-09-24 19:41:37 +0200

    added live api environment

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-export const API_URL = 'https://staging.vendor.hehe.rw/api';
-export const API_ROOT_URL = 'https://staging.vendor.hehe.rw';
+export const API_URL = 'https://vendor-dashboard.hehe.rw/api';
+export const API_ROOT_URL = 'https://vendor-dashboard.hehe.rw';
 export const TOKEN_KEY = 'VENDOR_TOKEN';
 export const AUTH_USER_LOCAL_STORAGE_INFO = 'AUTH_USER_LOCAL_INFO';
 export const PLATFORM_CLIENT_ID = 2;
@@ -7,7 +7,7 @@ export const PLATFORM_CLIENT_SECRET = 'sy71st8Eg4PjgCpHGq25kmC5EbrRvdrF30DSemgL'
 export const COUNTRY_API_URL = 'http://battuta.medunes.net/api';
 export const COUNTRY_API_KEY = '46545b12aaa7048c8390dfe80c396937';
 export const ORDER_DATA_TOKEN = 'ORDER_DATA';
-export const APP_DOMAIN='https://release.vendor.hehe.rw';
+export const APP_DOMAIN='https://m.hehe.rw';
 
 // local storage variables
 export const CART_ITEMS_KEY = 'CART_ITEMS';
