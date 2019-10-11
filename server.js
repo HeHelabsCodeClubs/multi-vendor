96f0823364b7a5750ace19a3af9acccf0173fbdb
christophe12 <mutachristophe12@gmail.com>
2019-10-11 16:41:43 +0200

    adding mobile redirection

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = next({ dev })
 const handle = app.getRequestHandler()
 const port = app_conf.env.port;
 const getTokenValue = app_conf.env.getTokenValue;
+const mobileSiteUrl = 'https://m.hehe.rw';
 
 
 app
@@ -15,18 +16,39 @@ app
         const server = express()
 
         server.get('/order-complete/:payment', (req, res) => {
+            const ua = req.header('user-agent');
+            if(/mobile|iphone|ipod|android|blackberry|opera|mini|windows\sce|palm|smartphone|iemobile|ipad|android|android 3.0|xoom|sch-i800|playbook|tablet|kindle/i.test(ua)) {
+                res.writeHead(302, {
+                    Location: mobileSiteUrl + '/order-complete/' + req.params.payment
+                });
+                res.end();
+            }
             return app.render(req, res, '/order-complete', { 
                 payment: req.params.payment
             });
         })
 
         server.get('/process/:payment', (req, res) => {
+            const ua = req.header('user-agent');
+            if(/mobile|iphone|ipod|android|blackberry|opera|mini|windows\sce|palm|smartphone|iemobile|ipad|android|android 3.0|xoom|sch-i800|playbook|tablet|kindle/i.test(ua)) {
+                res.writeHead(302, {
+                    Location: mobileSiteUrl + '/process/' + req.params.payment
+                });
+                res.end();
+            }
             return app.render(req, res, '/process', { 
                 payment: req.params.payment
             });
         })
 
         server.get('/sellers/:seller/products/:slug', (req, res) => {
+            const ua = req.header('user-agent');
+            if(/mobile|iphone|ipod|android|blackberry|opera|mini|windows\sce|palm|smartphone|iemobile|ipad|android|android 3.0|xoom|sch-i800|playbook|tablet|kindle/i.test(ua)) {
+                res.writeHead(302, {
+                    Location: mobileSiteUrl + '/sellers/' + req.params.seller + '/' + req.params.slug
+                });
+                res.end();
+            }
             return app.render(req, res, '/product-page', { 
                 seller: req.params.seller,
                 slug: req.params.slug
@@ -34,6 +56,13 @@ app
         })
 
         server.get('/search-results/:search_term', (req, res) => {
+            const ua = req.header('user-agent');
+            if(/mobile|iphone|ipod|android|blackberry|opera|mini|windows\sce|palm|smartphone|iemobile|ipad|android|android 3.0|xoom|sch-i800|playbook|tablet|kindle/i.test(ua)) {
+                res.writeHead(302, {
+                    Location: mobileSiteUrl + '/search-results/' + req.params.search_term
+                });
+                res.end();
+            }
             if (!req.params.search_term || (req.params.search_term === '')) {
                 res.redirect('/');
             }
@@ -43,12 +72,26 @@ app
         })
 
         server.get('/categories/:category_slug', (req, res) => {
+            const ua = req.header('user-agent');
+            if(/mobile|iphone|ipod|android|blackberry|opera|mini|windows\sce|palm|smartphone|iemobile|ipad|android|android 3.0|xoom|sch-i800|playbook|tablet|kindle/i.test(ua)) {
+                res.writeHead(302, {
+                    Location: mobileSiteUrl + '/categories/' + req.params.category_slug
+                });
+                res.end();
+            }
             return app.render(req, res, '/categories', { 
                 category_slug: req.params.category_slug
             });
         })
 
         server.get('/categories/:category_slug/:sub_cat_slug', (req, res) => {
+            const ua = req.header('user-agent');
+            if(/mobile|iphone|ipod|android|blackberry|opera|mini|windows\sce|palm|smartphone|iemobile|ipad|android|android 3.0|xoom|sch-i800|playbook|tablet|kindle/i.test(ua)) {
+                res.writeHead(302, {
+                    Location: mobileSiteUrl + '/categories/' + req.params.category_slug + '/' + req.params.sub_cat_slug
+                });
+                res.end();
+            }
             return app.render(req, res, '/categories', { 
                 category_slug: req.params.category_slug,
                 sub_cat_slug: req.params.sub_cat_slug
@@ -56,6 +99,13 @@ app
         })
 
         server.get('/categories/:category_slug/:sub_cat_slug/:sub_last_cat_slug', (req, res) => {
+            const ua = req.header('user-agent');
+            if(/mobile|iphone|ipod|android|blackberry|opera|mini|windows\sce|palm|smartphone|iemobile|ipad|android|android 3.0|xoom|sch-i800|playbook|tablet|kindle/i.test(ua)) {
+                res.writeHead(302, {
+                    Location: mobileSiteUrl + '/categories/' + req.params.category_slug + '/' + req.params.sub_cat_slug + '/' + req.params.sub_last_cat_slug
+                });
+                res.end();
+            }
             return app.render(req, res, '/categories', { 
                 category_slug: req.params.category_slug,
                 sub_cat_slug: req.params.sub_cat_slug,
@@ -64,12 +114,26 @@ app
         })
 
         server.get('/invoice/:id', (req, res) => {
+            const ua = req.header('user-agent');
+            if(/mobile|iphone|ipod|android|blackberry|opera|mini|windows\sce|palm|smartphone|iemobile|ipad|android|android 3.0|xoom|sch-i800|playbook|tablet|kindle/i.test(ua)) {
+                res.writeHead(302, {
+                    Location: mobileSiteUrl + '/invoice/' + req.params.id
+                });
+                res.end();
+            }
             return app.render(req, res, '/invoice', { 
                 id: req.params.id
             });
         })
 
         server.get('/profile/:page', (req, res) => {
+            const ua = req.header('user-agent');
+            if(/mobile|iphone|ipod|android|blackberry|opera|mini|windows\sce|palm|smartphone|iemobile|ipad|android|android 3.0|xoom|sch-i800|playbook|tablet|kindle/i.test(ua)) {
+                res.writeHead(302, {
+                    Location: mobileSiteUrl + '/profile/' + req.params.page
+                });
+                res.end();
+            }
             const token = getTokenValue(req.headers.cookie, 'VENDOR_TOKEN');
             if (!token && req.params.page === 'orders') {
                 res.redirect('/signin');
@@ -80,6 +144,13 @@ app
         })
 
         server.get('/checkout/:page', (req, res) => {
+            const ua = req.header('user-agent');
+            if(/mobile|iphone|ipod|android|blackberry|opera|mini|windows\sce|palm|smartphone|iemobile|ipad|android|android 3.0|xoom|sch-i800|playbook|tablet|kindle/i.test(ua)) {
+                res.writeHead(302, {
+                    Location: mobileSiteUrl + '/checkout/' + req.params.page
+                });
+                res.end();
+            }
             const token = getTokenValue(req.headers.cookie, 'VENDOR_TOKEN');
             if (token && req.params.page === 'account') {
                 res.redirect('/checkout/addresses');
@@ -95,6 +166,13 @@ app
         })
 
         server.get('/p/:id', (req, res) => {
+            const ua = req.header('user-agent');
+            if(/mobile|iphone|ipod|android|blackberry|opera|mini|windows\sce|palm|smartphone|iemobile|ipad|android|android 3.0|xoom|sch-i800|playbook|tablet|kindle/i.test(ua)) {
+                res.writeHead(302, {
+                    Location: mobileSiteUrl + '/p/' + req.params.id
+                });
+                res.end();
+            }
             const actualPage = '/post'
             const queryParams = { title: req.params.id }
             app.render(req, res, actualPage, queryParams)
@@ -104,7 +182,7 @@ app
             const ua = req.header('user-agent');
             if(/mobile|iphone|ipod|android|blackberry|opera|mini|windows\sce|palm|smartphone|iemobile|ipad|android|android 3.0|xoom|sch-i800|playbook|tablet|kindle/i.test(ua)) {
                 res.writeHead(302, {
-                    Location: 'https://m.hehe.rw'
+                    Location: mobileSiteUrl
                 });
                 res.end();
             }
